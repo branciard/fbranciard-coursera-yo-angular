f174fc978da3414b4c20576ebe8a42192ef36ab7
branciard <francoisbranciard@gmail.com>
2016-06-12 14:50:13 +0200

    init

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fbranciardCourseraYoAngularApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the fbranciardCourseraYoAngularApp
+ */
+angular.module('fbranciardCourseraYoAngularApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fbranciardCourseraYoAngularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the fbranciardCourseraYoAngularApp
+ */
+angular.module('fbranciardCourseraYoAngularApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
